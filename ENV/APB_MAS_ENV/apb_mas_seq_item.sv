3a151afb445b89b69a50d1624aed1f1d766131a7
Aditya Mishra <140566035+adit0131@users.noreply.github.com>
2023-10-10 11:43:24 +0530

    add some upadted file.

--- ENV/APB_MAS_ENV/apb_mas_seq_item.sv
+++ ENV/APB_MAS_ENV/apb_mas_seq_item.sv
@@ -2,7 +2,7 @@
 // Company        : SCALEDGE 
 // Engineer       : Aditya Mishra 
 // Create Date    : Wed Sep 27 13:54:02 2023
-// Last Modified  : Tue Oct 10 10:13:24 2023
+// Last Modified  : Tue Oct 10 11:42:04 2023
 // Modified By    : Aditya Mishra 
 // File Name   	  : apd_mas_seq_item.sv
 // Class Name 	  : apb_mas_seq_item  
@@ -36,7 +36,34 @@ class apb_mas_seq_item extends uvm_sequence_item;
   rand bit [(`APB_STROB_WIDTH-1):0]  apb_pstrob;
   rand bit [2:0]                     apb_pprot;
 
-  
+//----------------------------------------------------------------------------
+// Function : do_print
+//----------------------------------------------------------------------------
+ virtual function void do_print (uvm_printer printer);
+   super.do_print(printer);
+
+ endfunction
+
+//----------------------------------------------------------------------------
+// Function : do_copy
+//----------------------------------------------------------------------------
+  virtual function void do_copy (uvm_object rhs);
+    apb_mas_seq_item rhs_;
+    super.do_copy(rhs);
+    $cast(rhs_,rhs);
+
+  endfunction
+
+//----------------------------------------------------------------------------
+// Function : do_compare
+//----------------------------------------------------------------------------
+  virtual function void do_compare (uvm_object rsh,uvm_comparer comparer);  
+    apb_mas_seq_item rsh_;
+    do_compare = super.do_compare(rsh,comparer);
+    $cast(rhs_,rhs);
+
+  endfunction
+
 endclass
 
 `endif 
